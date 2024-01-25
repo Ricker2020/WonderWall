a47c91344f825d4331597f7128f62e5806f5dd0b
unknown <enrique_piedra2001@hotmail.com>
2024-01-25 13:59:46 -0500

    alejo: Monedas Pista 3 sol

--- Diversion/Scripts/Pista3CollitionAction.cs
+++ Diversion/Scripts/Pista3CollitionAction.cs
@@ -5,29 +5,38 @@ using UnityEngine;
 public class Pista3CollitionAction : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    void Start()
+    {
 
-     private void OnTriggerEnter(Collider other){
-    // Comprobar si el objeto colisionado tiene la etiqueta
-        if (other.gameObject.tag=="PlayerDiversion"){
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprobar si el objeto colisionado tiene la etiqueta
+        if (other.gameObject.tag == "PlayerDiversion")
+        {
             Collider thisCollider = GetComponent<Collider>();
-            if(thisCollider.gameObject.tag=="CoinPista3_1"){
-                Variables_Diversion.instance.monedas+=1;
-                Variables_Diversion.instance.coin1=false;
+            if (thisCollider.gameObject.tag == "CoinPista3_1")
+            {
+                Variables_Diversion.instance.monedas += 1;
+                Variables_Diversion.instance.coin1 = false;
             }
-            else if(thisCollider.gameObject.tag=="CoinPista3_2"){
-                Variables_Diversion.instance.monedas+=1;
-                Variables_Diversion.instance.coin2=false;
+            else if (thisCollider.gameObject.tag == "CoinPista3_2")
+            {
+                Variables_Diversion.instance.monedas += 1;
+                Variables_Diversion.instance.coin2 = false;
             }
-            else if(thisCollider.gameObject.tag=="CoinPista3_3"){
-                Variables_Diversion.instance.monedas+=1;
-                Variables_Diversion.instance.coin3=false;
+            else if (thisCollider.gameObject.tag == "CoinPista3_3")
+            {
+                Variables_Diversion.instance.monedas += 1;
+                Variables_Diversion.instance.coin3 = false;
             }
-            else{
-                Variables_Diversion.instance.collision=true;
+            else
+            {
+                Variables_Diversion.instance.collision = true;
             }
 
-                  
+
             //prueba.text+=thisCollider.gameObject.tag;
         }
     }
